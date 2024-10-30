b523d78bda58cf1031200c65965019207abe48b9
Marquezallen51 <Marquezallen51@gmail.com>
2024-10-29 22:58:11 -0700

    Fixing timer

--- Assets/GameManager/Game Scenes/Seeding/Scripts/Phase 1 Manager.cs
+++ Assets/GameManager/Game Scenes/Seeding/Scripts/Phase 1 Manager.cs
@@ -12,18 +12,21 @@ public class TimeManager : MonoBehaviour
     public Text scoreText;
     public float currTime;
     public float phaseOneScore;
+    public int currTimeInt;
     // Start is called before the first frame update
     void Start()
     {
         currTime = timer.currentTime;
         phaseOneScore = 0;
+        currTimeInt = (int)currTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeText.text = "Time: " + currTime.ToString();
+        timeText.text = "Time: " + currTimeInt.ToString();
         currTime = timer.currentTime;
+        currTimeInt= (int)currTime;
         phaseOneScore = p1.scoreTotal;
         scoreText.text = "Score: " + phaseOneScore.ToString();
     }
